75575e0a7575056724377a8b0e1768546ada7010
Niyas Sajjad <niyas.developer@gmail.com>
2017-07-20 00:39:03 +0530

    added basic home layout,navigation from splash to home

--- app/src/main/java/com/niyas/redditlikeapp/activities/SplashActivity.java
+++ app/src/main/java/com/niyas/redditlikeapp/activities/SplashActivity.java
@@ -1,14 +1,25 @@
 package com.niyas.redditlikeapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.niyas.redditlikeapp.R;
 
 public class SplashActivity extends BaseActivity {
+    private final int SPLASH_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_DELAY);
     }
 }
