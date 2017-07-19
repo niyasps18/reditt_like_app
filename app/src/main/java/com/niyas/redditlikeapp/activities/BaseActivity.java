78917da2ebf9a6a3bcd5fbdebe4ea97d8676ba67
Niyas Sajjad <niyas.developer@gmail.com>
2017-07-20 00:21:29 +0530

    added splash,butterknife library

---
+++ app/src/main/java/com/niyas/redditlikeapp/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.niyas.redditlikeapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Niyas Sajjad on 19-Jul-17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/niyas/redditlikeapp/models/RedditPostModel.java
@@ -0,0 +1,8 @@
+package com.niyas.redditlikeapp.models;
+
+/**
+ * Created by Niyas Sajjad on 19-Jul-17.
+ */
+
+public class RedditPostModel {
+}
